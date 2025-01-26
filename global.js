3d1bf0d403692c6f1f9a5a53b5fea12cf4e4fc55
zijin-qin <zijin.qin@gmail.com>
2025-01-26 00:54:05 -0800

    Update global.js

--- global.js
+++ global.js
@@ -13,10 +13,10 @@ function $$(selector, context = document) {
 // currentLink?.classList.add('current');
 
 let pages = [
-    { url: 'portfolio/', title: 'Home' },
-    { url: 'portfolio/projects/', title: 'Projects' },
-    { url: 'portfolio/contact/', title: 'Contact Me'},
-    { url: 'portfolio/resume/', title: 'Resume'},
+    { url: '', title: 'Home' },
+    { url: 'projects/', title: 'Projects' },
+    { url: 'contact/', title: 'Contact Me'},
+    { url: 'resume/', title: 'Resume'},
     { url: 'https://github.com/zijin-qin', title: 'My GitHub'}
 ];
 
