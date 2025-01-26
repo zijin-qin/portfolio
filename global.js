482c38e9a3e931e31d2f9fe9acbf8f5ff49b9154
zijin-qin <zijin.qin@gmail.com>
2025-01-25 23:32:37 -0800

    JS update

--- global.js
+++ global.js
@@ -3,3 +3,72 @@ console.log('IT’S ALIVE!');
 function $$(selector, context = document) {
   return Array.from(context.querySelectorAll(selector));
 }
+
+// let navLinks = $$("nav a");
+
+// let currentLink = navLinks.find(
+//     (a) => a.host === location.host && a.pathname === location.pathname
+//   );
+
+// currentLink?.classList.add('current');
+
+let pages = [
+    { url: '', title: 'Home' },
+    { url: 'projects/', title: 'Projects' },
+    { url: 'contact/', title: 'Contact Me'},
+    { url: 'resume/', title: 'Resume'},
+    { url: 'https://github.com/zijin-qin', title: 'My GitHub'}
+];
+
+let nav = document.createElement('nav');
+document.body.prepend(nav);
+
+for (let p of pages) {
+    let url = p.url;
+    let title = p.title;
+
+    const ARE_WE_HOME = document.documentElement.classList.contains('home');
+    url = !ARE_WE_HOME && !url.startsWith('http') ? '../' + url : url;
+
+    // nav.insertAdjacentHTML('beforeend', `<a href="${url}">${title}</a>`);
+    let a = document.createElement('a');
+    a.href = url;
+    a.textContent = title;
+    nav.append(a);
+
+    if (a.host === location.host && a.pathname === location.pathname) {
+        a.classList.add('current');
+    }
+
+    // Open external link in new tab
+    if (a.host !== location.host) {
+        a.target = "_blank";
+    }      
+}
+
+document.body.insertAdjacentHTML(
+    'afterbegin',
+    `
+      <label class="color-scheme">
+          Theme:
+          <select>
+                <option value="light dark">Automatic</option>
+                <option value="light">Light</option>
+                <option value="dark">Dark</option>
+          </select>
+      </label>`
+);
+
+let select = document.querySelector('.color-scheme');
+
+if (localStorage.colorScheme) {
+    document.documentElement.style.setProperty('color-scheme', localStorage.colorScheme);
+    select.value = localStorage.colorScheme;
+}
+
+select.addEventListener('input', function (event) {
+    console.log('color scheme changed to', event.target.value);
+    document.documentElement.style.setProperty('color-scheme', event.target.value);
+
+    localStorage.colorScheme = event.target.value;
+});
